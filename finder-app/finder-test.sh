c4227e22fadc1e02801a7b96c1302e648daeb317
nabilah <Ahmeddnabil97@gmail.com>
2024-02-13 01:53:37 +0200

    update with native compiler

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -50,7 +50,7 @@ then
 fi
 #echo "Removing the old writer utility and compiling as a native application"
 make clean
-make CROSS_COMPILE=aarch64-none-linux-gnu-
+make 
 
 for i in $( seq 1 $NUMFILES)
 do
